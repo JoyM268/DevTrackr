496767dafe8e37de8efee2db1d423926f8e9fd11
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-12-05 06:56:21 +0530

    Updated Button To Be Responsive

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
 	return (
-		<button className="bg-[#172a70] p-3 h-[70px] rounded-3xl border-[#c2b6ff] border border-solid hover:scale-105 transition-all duration-200 md:h-[50px]">
+		<button className="bg-[#172a70] p-3 h-[50px] rounded-3xl border-[#c2b6ff] border border-solid hover:scale-105 transition-all duration-200 md:h-[50px] text-sm md:text-base">
 			{children}
 		</button>
 	);
